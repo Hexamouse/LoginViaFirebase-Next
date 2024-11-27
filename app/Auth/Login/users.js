90f93590dd0cb4a37d5548408d271862bebd2b37
Kontolistik <151897038+Hexamouse@users.noreply.github.com>
2024-11-27 12:14:02 +0700

    mengupdatekan

--- app/Auth/Login/users.js
+++ app/Auth/Login/users.js
@@ -1,8 +1,6 @@
-// app/Auth/Login/UsersFetcher.js
 import fs from 'fs';
 import path from 'path';
 
-// Fungsi untuk mengambil data pengguna dari file JSON
 export async function fetchUsers() {
   const filePath = path.join(process.cwd(), 'data', 'users.json');
   const fileData = fs.readFileSync(filePath, 'utf8');
