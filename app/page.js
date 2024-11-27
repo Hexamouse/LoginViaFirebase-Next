8466002f1b378c2304dc2e321e60c705494ccea1
Kontolistik <151897038+Hexamouse@users.noreply.github.com>
2024-11-27 12:15:38 +0700

    jir

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-// app/page.js
 'use client';
 
 import { useEffect } from 'react';
@@ -8,9 +7,8 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    // Redirect otomatis ke halaman login
     router.push('/Auth/Login');
   }, [router]);
 
-  return null; // Tidak ada tampilan, karena langsung di-redirect
+  return null;
 }
